7f46e26849af33548e5965bda61255554af682f2
Wade Dismukes <wade.dismukes@gmail.com>
2020-05-05 10:13:24 -0500

    working function for summary statistics complete. need documentation.

--- R/genetree_summary_stat.R
+++ R/genetree_summary_stat.R
@@ -9,13 +9,23 @@ genetree_summary_stat <- function(locus_tree_gene_tree_obj, locus_tree_indx){
      gamma <- vector(length = num_genetrees)
      sackin <- vector(length = num_genetrees)
      tmrca <- vector(length = num_genetrees)
+     cherries <- vector(length = num_genetrees)
      for(i in 1:num_genetrees){
-         colless[i] <- apTreeshape::colless(genetrees[[i]])
+         colless[i] <- apTreeshape::colless(apTreeshape::as.treeshape.phylo(genetrees[[i]]))
          gamma[i] <- ape::gammaStat(genetrees[[i]])
-         sackin[i] <- apTreeshape::sackin(genetrees[[i]])
+         cherries[i] <- treeducken::cherry(genetrees[[i]])
+         sackin[i] <- apTreeshape::sackin(apTreeshape::as.treeshape.phylo(genetrees[[i]]))
          tmrca[i] <- max(phytools::nodeHeights(genetrees[[i]]))
      }
-     data.frame(colless, sackin, tmrca, gamma_locus, gamma)
+     data.frame(colless, sackin, tmrca, gamma_locus, gamma, cherries)
 }
 
 
+# this is copied from ape::cherry directly, I have copied it here because
+# I wanted a return value of the statistic rather than the test itself
+# copyright Emmanuel Paradis
+# used under GNU public license
+cherry <- function(phy){
+    n <- length(phy$tip.label)
+    sum(tabulate(phy$edge[, 1][phy$edge[, 2] <= n]) == 2)
+}
