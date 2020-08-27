864c440320862a2ca70df340ce1e1bd7a2671c36
Wade Dismukes <wade.dismukes@gmail.com>
2020-08-27 17:43:36 -0500

    fixing multi-locus sim

--- R/sim_multilocus.R
+++ R/sim_multilocus.R
@@ -14,35 +14,16 @@ sim_multilocus_coal <- function(locus_tree,
                                 model = "depth",
                                 locus_tree_time_length * v)
 
-    if(!(any(grep("D[A-Z]", locus_tree$node.label)))) {
-        return(treeducken::sim_multispecies_coal(
-                                                locus_tree,
-                                                v,
-                                                num_sampled_individuals = ipp,
-                                                num_genes = num_genes))
-    }
-
     # split tree into subtrees at each duplication
-    loc_trees_subtree_all <- ape::subtrees(locus_tree)
-    ul_trees <- unlist(loc_trees_subtree_all, recursive = FALSE)
-    node_labels <- ul_trees[seq(from = 6, to = length(ul_trees), by = 7)]
-    indices_of_dup <- which(node_labels$node.label != "")
-    locus_trees_by_dup <- rev(loc_trees_subtree_all[c(1, indices_of_dup)])
+    locus_trees_by_dup <- seperate_into_loci(locus_tree)
 
-    # add an artificial root for treeducken
-    for(i in seq_len(length(locus_trees_by_dup) - 1)) {
-        locus_trees_by_dup[[i]]$root.edge <- 0.0
-    }
-
-    # get the tip labels of each locus created by duplications
-    num_locus <- length(locus_trees_by_dup)
-    ul_dup_loc_trees <- unlist(locus_trees_by_dup, recursive = FALSE)
-    tip_labels_dup_trees <- ul_dup_loc_trees[seq(from = 4, to = length(ul_dup_loc_trees), by = 8)]
     mlc_df <- list(length = length(locus_trees_by_dup))
     # loop through the locus trees simulate using MSC then prune the loci
-    # that have that locus as a subtree to one tip (to simulate that that coalescent
+    # that have that locus as a subtree to one tip (to simulate that
+    # that coalescent
     # event has occurred and won't affect the sims)
     for(i in seq_len(length(locus_trees_by_dup))) {
+        # add artificial root for treeducken
         if(is.null(locus_trees_by_dup[[i]]$root.edge)) {
             locus_trees_by_dup[[i]]$root.edge <- 0.0
         }
@@ -52,25 +33,72 @@ sim_multilocus_coal <- function(locus_tree,
                                                 v,
                                                 num_sampled_individuals = ipp,
                                                 num_genes = num_genes)
-        # which tips to remove
-        tips_to_remove <- locus_trees_by_dup[[i]]$tip.label[2:ape::Ntip(locus_trees_by_dup[[i]])]
-        # figure out which loci have those tips
-        subtrees_w_tips_to_drop <- which(
-                                        unlist(
-                                            lapply(
-                                                mapply(function(x,y) x %in% y,
-                                                       tip_labels_dup_trees,
-                                                       tips_to_remove)
-                                                , any)))
-        # loci to prune
-        trees_to_prune <- locus_trees_by_dup[subtrees_w_tips_to_drop]
-        # prune the loci making sure to keep the fake root
-        pruned_trees <- lapply(trees_to_prune,
-                               function(x) ape::drop.tip(x,
-                                                         tip = tips_to_remove,
-                                                         rooted = TRUE))
-        # update the list of locus trees
-        locus_trees_by_dup[subtrees_w_tips_to_drop] <- pruned_trees
+        # collapse those loci on all
+        locus_trees_by_dup <- collapse_locus_subtree(locus_trees_by_dup,
+                                                    locus_trees_by_dup[[i]])
     }
     mlc_df
+}
+
+
+seperate_into_loci <- function(locus_tree) {
+    if(!(any(grep("D[A-Z]", locus_tree$node.label)))) {
+        return(locus_tree)
+    }
+
+    # split tree into subtrees at each duplication
+    loc_trees_subtree_all <- ape::subtrees(locus_tree)
+    ul_trees <- unlist(loc_trees_subtree_all, recursive = FALSE)
+    node_labels <- ul_trees[which(names(ul_trees) == "node.label")]
+    indices_of_dup <- which(node_labels$node.label != "")
+    rev(loc_trees_subtree_all[c(1, indices_of_dup)])
+
+}
+# SUBTREES
+# given locus tree with duplications points marked with D + whatever
+# give daughter trees + mother tree
+
+# LOOP THROUGH SUBTREES
+
+
+# COALESCE
+
+
+# COLLAPSE THAT SUBTREE ON TREES CONTAINING THAT SUBTREE
+collapse_locus_subtree <- function(list_of_subtrees,
+                                   locus_to_collapse) {
+    tip_labels_subtrees <- get_tip_labels_tree_list(list_of_subtrees)
+    tips_to_remove <- locus_to_collapse$tip.label[-1]
+        # figure out which loci have those tips
+
+    tree_indices_to_drop <- lapply(tip_labels_subtrees,
+                                   function(x) x %in% tips_to_remove)
+
+    self_check <-  which(
+                        unlist(
+                            lapply(tree_indices_to_drop, all))
+                    , useNames = FALSE)
+    if(length(self_check) != 0)
+        tree_indices_to_drop <- tree_indices_to_drop[-self_check]
+
+    tree_indices_to_drop <- which(
+                                unlist(
+                                    lapply(tree_indices_to_drop, any
+                            , useName = FALSE)))
+    trees_to_prune <- list_of_subtrees[tree_indices_to_drop]
+    pruned_trees <- suppressWarnings(lapply(trees_to_prune,
+                                function(x) ape::drop.tip(x,
+                                                         tip = tips_to_remove,
+                                                         rooted = TRUE)))
+    list_of_subtrees[tree_indices_to_drop] <- pruned_trees
+    list_of_subtrees
+}
+
+get_tip_labels <- function(tree) {
+    tree$tip.label
+}
+
+get_tip_labels_tree_list <- function(multi_tree) {
+    ul_multi_tree <- unlist(multi_tree, recursive = FALSE)
+    ul_multi_tree[which(names(ul_multi_tree) == "tip.label")]
 }
